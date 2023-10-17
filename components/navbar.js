e90d7f9fd51103c74dea7e628cef0b8d2c55d9ef
gooeye <uniconpatio@gmail.com>
2023-10-17 17:07:47 +0800

    switched to vue3, added props
    profile and logout can be toggled with isLoggedIn and active page can be changed

--- components/navbar.js
+++ components/navbar.js
@@ -1,6 +1,10 @@
-Vue.component('vertical-navbar', {
+const navbar = {
+    props: {
+        'page' : String,
+        'isLoggedIn' : Boolean,
+    },
     template: `
-        <nav class="col-2 vh-100 sticky-top p-0 d-flex justify-content-between flex-column">
+        <nav class="col-2 vh-100 sticky-top p-0 d-flex justify-content-between flex-column overflow-scroll">
             <div class="container">
                 <a href="/" class="navbar-brand d-block my-4 mx-auto text-center link-light text-decoration-none">
                     <i class="bi bi-egg me-1 fs-3"></i>
@@ -14,7 +18,7 @@ Vue.component('vertical-navbar', {
                 </div>
                 <ul class="nav nav-pills flex-column mb-auto">
                     <li class="nav-item">
-                        <a class="nav-link active" href="#">
+                        <a :class="{'nav-link':true, 'active':page=='home'}" href="index.html">
                             <i class="bi bi-house me-2"></i>
                             <span class="align-middle">Home</span>
                         </a>
@@ -23,13 +27,13 @@ Vue.component('vertical-navbar', {
                         Discover
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">
+                        <a :class="{'nav-link':true, 'active':page=='drinks'}" href="drinks.html">
                             <i class="bi bi-cup-straw me-2"></i>
                             <span class="align-middle">Drinks</span>
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">
+                        <a :class="{'nav-link':true, 'active':page=='events'}" href="events.html">
                             <i class="bi bi-calendar-event me-2"></i>
                             <span class="align-middle">Events</span>
                         </a>
@@ -38,13 +42,13 @@ Vue.component('vertical-navbar', {
                         Community
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">
+                        <a :class="{'nav-link':true, 'active':page=='friends'}" href="friends.html">
                             <i class="bi bi-people me-2"></i>
                             <span class="align-middle">Friends</span>
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">
+                        <a :class="{'nav-link':true, 'active':page=='chats'}" href="chats.html">
                             <i class="bi bi-chat-left-dots me-2"></i>
                             <span class="align-middle">Chats</span>
                         </a>
@@ -53,13 +57,13 @@ Vue.component('vertical-navbar', {
                         Concoctions
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">
+                        <a :class="{'nav-link':true, 'active':page=='concoctions'}" href="my-concoctions.html">
                             <i class="bi bi-list-nested me-2"></i>
                             <span class="align-middle">My Concoctions</span>
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">
+                        <a :class="{'nav-link':true, 'active':page=='create'}" href="create-a-concoction.html">
                             <i class="bi bi-plus-square me-2"></i>
                             <span class="align-middle">Create a concoction</span>
                         </a>
@@ -68,20 +72,30 @@ Vue.component('vertical-navbar', {
             </div>
             <div class="container">
                 <ul class="nav nav-pills flex-column mb-3">
-                    <li class="nav-item text">
-                        <a class="nav-link" href="#">
-                            <img class="img profile" src="https://t4.ftcdn.net/jpg/05/62/99/31/360_F_562993122_e7pGkeY8yMfXJcRmclsoIjtOoVDDgIlh.jpg"></i>
+                    <li :class="{'nav-item':true, 'd-none':!isLoggedIn}">
+                        <a :class="{'nav-link':true, 'active':page=='profile'}" href="profile.html">
+                            <img class="img profile" src="https://t4.ftcdn.net/jpg/05/62/99/31/360_F_562993122_e7pGkeY8yMfXJcRmclsoIjtOoVDDgIlh.jpg">
                             <span class="align-middle">Profile</span>
                         </a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">
+                    <li :class="{'nav-item':true, 'd-none':!isLoggedIn}">
+                        <a :class="{'nav-link':true}" href="#">
                             <i class="bi bi-plus-square me-2"></i>
                             <span class="align-middle">Logout</span>
+                        </a>
+                    </li>
+                    <li :class="{'nav-item':true, 'd-none':isLoggedIn}">
+                        <a class="nav-link text-center" href="#">
+                            <span class="align-middle">Log in</span>
+                        </a>
+                    </li>
+                    <li :class="{'nav-item':true, 'my-1':true, 'd-none':isLoggedIn}">
+                        <a class="nav-link text-center active" href="#">
+                            <span class="align-middle">Create an account</span>
                         </a>
                     </li>
                 </ul>
             </div>
         </nav>
     `
-});
+};

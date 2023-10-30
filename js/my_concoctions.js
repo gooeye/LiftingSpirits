5ab8de56ac77287652364e0b952aa98a29cf674b
elliot <elliot.tan.2022@scis.smu.edu.sg>
2023-10-30 16:44:54 +0800

    asd

---
+++ js/my_concoctions.js
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp }  from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
+import { getAuth, onAuthStateChanged, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-auth.js";
+import { getFirestore,collection, setDoc, doc, addDoc} from "https://www.gstatic.com/firebasejs/10.5.0/firebase-firestore.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyByL2UivS9auwkM5vyc7REfo3uAjyjq_E0",
+  authDomain: "liftingspirits1.firebaseapp.com",
+  projectId: "liftingspirits1",
+  storageBucket: "liftingspirits1.appspot.com",
+  messagingSenderId: "223340916076",
+  appId: "1:223340916076:web:6897d475b8789b0e8ad749",
+  measurementId: "G-1EDMKV8YRT",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const db = getFirestore(app);
+
+//get drinks created by the user
+
+

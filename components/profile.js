63ca2d81ec11acb384967d50cb5a0c4d104ef896
minthukha12 <minthukha2002@gmail.com>
2023-11-05 22:58:41 +0800

    t

--- components/profile.js
+++ components/profile.js
@@ -14,6 +14,16 @@ export const user = {
         return {
             selected: [],
             showModal: false,
+            totalCalories: 0,
+            items: [
+                { name: "Beer, 4%", calories: 180, count: 0, path: "Pint" },
+                { name: "Beer, 5%", calories: 142.5, count: 0, path: "Beer" },
+                { name: "Cider, 4.5%", calories: 204.5, count: 0, path: "Cider" },
+                { name: "Wine, 13%", calories: 149.75, count: 0, path: "Wine" },
+                { name: "Champagne, 12%", calories: 132, count: 0, path: "Champagne" },
+                { name: "Spirits, 40%", calories: 56, count: 0, path: "Spirits" },
+                { name: "Alcopops, 4%", calories: 110, count: 0, path: "Alcopops" }
+            ],
             drinks: [
                 {
                     img: "https://www.petitefleursg.com//image/cache/catalog/271376_pizzolato_ros_extra_dry_m-use_nv_pp-700x700.jpg",
@@ -76,6 +86,10 @@ export const user = {
     methods: {
         select() {
             this.selected = document.querySelectorAll('.drinkSel:checked')
+        },
+        onItemClick(item) {
+            item.count++;
+            this.totalCalories += item.calories;
         }
     },
     computed: {
@@ -170,7 +184,49 @@ export const user = {
                 </table>
             </div>
             <div class="container rounded-3 background3 p-4 shadow-sm mb-4" id="tracking" >
-    <tracking></tracking>
+            <div class="text-center mb-4">
+            <h1 class="display-4">Unit and Calorie Tracking</h1>
+            <p class="subText text-muted">You are advised to drink less than 14 units a week</p>
+        </div>
+        <div class="row row-cols-md-2 row-cols-sm-1">
+            <div class="col justify-content-center">
+                <div class="calorieTable">
+                    <div class="container">
+                        <div class="row justify-content-center row-cols-md-2 row-cols-sm-1">
+                            <!-- Your existing HTML content for drink items goes here -->
+                            <div v-for="(item, index) in items" :key="index" class="col-md-4 itemContainer" @click="onItemClick(item)">
+                                <div class="col">
+                                    <img :src="'../components/img/' + item.path + '.png'" :alt="item.name + '.png'" class="mb-2">
+                                    <h3>{{ item.name }}</h3>
+                                    <p>{{ item.calories }} calories</p>
+                                    <span class="click-count">{{ item.count }}</span>
+                                </div>
+                            </div>
+                            <!-- ... -->
+
+                            <!-- Total Calories Display -->
+                            <div class="total-calories text-center">
+                                <h2 class="mb-3">Total Calories: <span class="text-danger">{{ totalCalories.toFixed(2) }}</span></h2>
+                            </div>
+
+                            <!-- Equivalent Comparison -->
+                            <div class="equivalent-section text-center">
+                                <h2 class="mb-4">Equivalent Comparison</h2>
+                                <div class="equivalent-item">
+                                    <div class="text-light">Number of Cheeseburgers:</div>
+                                    <div class="font-weight-bold">{{ (totalCalories / 250).toFixed(2) }}</div>
+                                </div>
+                                <div class="equivalent-item">
+                                    <div class="text-light">Minutes of Running:</div>
+                                    <div class="font-weight-bold">{{ (totalCalories / 10).toFixed(2) }}</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- ... -->
+        </div>
 </div>
         </div>
     `,

7e6a7ae013d8aca602837c1f8c27a85dcc3ca5fa
gooeye <uniconpatio@gmail.com>
2023-11-07 02:42:41 +0800

    connected events to db

--- js/util.js
+++ js/util.js
@@ -1,9 +1,9 @@
-import { getStorage, ref, uploadBytes } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-storage.js";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-storage.js";
 
 export function uploadImage(id, folder, fileName) {
+    const storage = getStorage();
     const fileInput = document.getElementById(id);
     const file = fileInput.files[0];
-    const storage = getStorage();
     var filePath = fileInput.value.split('.');
     var extension = filePath[filePath.length-1]
     const storageRef = ref(storage, folder+"/"+fileName+"."+extension);
@@ -12,4 +12,14 @@ export function uploadImage(id, folder, fileName) {
         console.log('Uploaded a blob or file!');
     });
     return folder+"/"+fileName+"."+extension;
+}
+
+export async function getUrl(file) {
+    const storage = getStorage();
+    try {
+        const url = await getDownloadURL(ref(storage, file))
+        return url
+    } catch (e) {
+        console.log(e)
+    }
 }
